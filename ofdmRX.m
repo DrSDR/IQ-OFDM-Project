3a2d57c5be478283093d4edcf32fe4ebc5d3159e
DrSDR <95492581+DrSDR@users.noreply.github.com>
2024-08-24 15:16:41 -0400

    Add files via upload

---
+++ ofdmRX.m
@@ -0,0 +1,220 @@
+
+
+% ofdm decoder
+clear all
+close all
+clc
+
+
+FILTERSPEC = '.wav';
+TITLE = 'Pick a Recorded OFDM IQ wav file';
+FILE = 'D:\gnu_radio_work';
+
+[FILENAME, PATHNAME, FILTERINDEX] = uigetfile(FILTERSPEC, TITLE, FILE);
+xstring = [PATHNAME FILENAME];
+
+[x,fswav] = audioread(xstring);
+Tswave = 1/fswav;
+
+% tx bandwidth
+fs = 300e3;  % final sample rate to convert IQ file to 
+
+Nguard = 512;   % time between pulse and preamble fft data
+
+Nchar = 1024;  % make power of 2
+bits = Nchar * 8;
+N2 = bits/2;
+Nfft = bits + 1024;  % bits bins plus 2 512 guard bins 
+
+
+PreN = 8192;
+Pulseseconds = PreN / fs;
+
+sigtime = Nguard + 2*Nfft + 2*Nfft + 2*Nguard;
+
+
+
+
+
+x = x(:,1) + 1i*x(:,2);
+x = x.';
+
+pw = Pulseseconds * fswav;
+pw = round(pw);
+pwvec = ones(1,pw);
+xdet = fftfilt(pwvec,abs(x));
+figure(1)
+plot(abs(xdet))
+title('abs of tone pulse');
+[maxval,maxindex] = max(abs(xdet));
+
+a = maxindex - (pw - 1);
+b = maxindex;
+
+tonesig = x(a:b);
+figure(2)
+plot(real(tonesig))
+hold on
+plot(imag(tonesig))
+hold off
+
+% compute freq of tonesig
+td1 = [0 tonesig(1:end-1)]; % delay by one sample 
+td1 = conj(td1);
+z = tonesig .* td1;
+z = angle(z);
+z = (1/(2*pi)) * (z / Tswave);
+z = z(56:end-56);
+foffset = mean(z);
+
+% toneNfft = round(4*fswav);
+% tonesig = fft(tonesig,toneNfft);
+% tonesig = fftshift(tonesig);
+% 
+% fvec = [0:toneNfft - 1];  % 0 to tonefft
+% fvec = fvec / (toneNfft );  % 0 to 1
+% fvec = fvec  *  fswav;  % 0 to fswave
+% fvec = fvec - fswav/2 ;  % -fswave/2 to fswave/2
+% 
+% 
+% figure(22)
+% plot(fvec/1e3, abs(tonesig));
+% title('fft of tone signal');
+% xlabel('frequency khz');
+% [maxtone,toneindex] = max(abs(tonesig));
+% toneindex = toneindex - 1;
+% foffset = toneindex - toneNfft/2;
+% foffset = foffset * (fswav /toneNfft);
+
+
+n = length(x);
+t = [1:n]/fswav;
+x = x .* exp(1i*2*pi*-foffset*t);   % freq shift sig to zero hz
+
+
+
+
+figure(3)
+plot(real(x))
+hold on
+plot(imag(x))
+hold off
+
+
+n = gcd(fs,fswav);
+p = fs / n;
+q = fswav / n;
+x = resample(x,p,q);   % resample file to expected sample rate
+
+hpw = ones(1,PreN);
+xdet = filter(hpw,1,abs(x));
+[maxv, maxi] = max(abs(xdet));
+xstart = maxi ;
+xend = xstart + sigtime ;
+
+
+figure(32)
+xplot = abs(x) / max(abs(x));
+xplot = 20 * log10(xplot);
+yplot = length(xdet);
+yplot = zeros(1,yplot);
+yplot(maxi) = -40;
+plot(xplot)
+hold on
+plot(yplot)
+hold on
+
+
+
+
+
+x = x(xstart:xend);
+a = Nguard  + round(Nfft/2) + 1 ;
+b = a + Nfft   - 1 ;
+xp = x(a:b);
+
+a1 =  Nguard + 2*Nfft + round(Nfft/2) + 1 ;
+b2 = a1 + Nfft  - 1 ;
+xd = x(a1:b2);
+
+
+
+
+
+
+
+Xp = fft(xp);
+Xp = fftshift(Xp);   % pilot tones  amp = 1, phase = 0
+
+figure(6)
+plot(20*log10(abs(Xp)))
+title('preamble fft');
+hold on
+
+Xd = fft(xd);
+Xd = fftshift(Xd);       % data tones,  bit1 = 180 deg,  bit0 = 0 deg
+
+plot(20*log10(abs(Xd)))
+title('data fft');
+hold off
+
+
+
+
+
+
+det = angle(Xp ./ Xd);          % ratio to get delta phase diff
+det = abs(det);
+
+thres = pi/2;
+
+% det = ( det >= thres) .* 1   +  (det < thres) .* 0;
+det = det(513:end);
+det = det(1:bits);   % bits for char  
+
+figure(8)
+plot(det)
+
+
+det(det <= thres) = 1;
+det(det > thres) = 0;
+
+
+    figure(105)
+    plot(det)
+    title('decoded bits vector')
+
+
+
+det = reshape(det,8,[]);
+det = det';
+
+
+charmes = zeros(1,Nchar);
+w = [7:-1:0];
+w = 2.^w;
+
+
+for k = 1:Nchar
+    x = det(k,:);
+    x = x .* w;
+    x = sum(x);
+    charmes(k) = x;
+end
+
+
+
+    clc
+
+   xstr =  char(charmes)
+
+   msgbox(xstr,'replace')
+
+
+
+
+
+
+
+
+
---
+++ ofdmtest.m
@@ -0,0 +1,221 @@
+
+
+clear all
+close all
+
+
+% pulse at start for sync and freq offset correction
+N = 8192;  %number of samples for sync pulse
+pream = ones(1,N);
+fs = 300e3;  % sample rate of ofdm signal , bw = 100khz 
+
+% gap between sync pulse and xp data vector 
+guardN = 512;
+
+% two seconds of pause inserted at start and end of iq file 
+td = 2;
+tdsamples = round(td * fs);
+tdvec = zeros(1,tdsamples);
+
+
+% number of char for text message 
+Nchar = 1024;  % make power of 2,  number of char for text
+bits = Nchar * 8;
+Nfft = bits + 1024;  % bits plus guard freq bins, gbins 512 + 512 
+
+
+
+% make pilot vector with random phases 
+xp = 23 * randn(1,Nfft);
+
+
+
+% pilot vector with 512 guard on each side
+xp = exp(1i*xp);  % mag = 1, phases = random 
+xp(1:512) = 0;  % first 512 bins guard bins = 0 
+xp(end-511:end) = 0;  % last 512 bins guard bins = 0
+
+
+% get message from txt file
+fid = fopen('C:\SDR_Work\textfile.txt');
+mtxt = fread(fid);  % vector of bytes,  each char = a number, one char = one byte or 8 bits 
+fclose(fid);
+mtxt = mtxt';
+if length(mtxt) >= Nchar
+    mtxt = mtxt(1:Nchar);
+else
+    z = length(mtxt);
+    z = Nchar - z;
+    mtxt = [mtxt zeros(1,z)];
+end
+mbits = dec2bin(mtxt,8);   %  Nchar x 8 matrix 
+mbits = reshape(mbits',1,[]);  % 1 x nbits  vector 
+mbits = mbits(1:bits);  % 1010101010101 as string vector  
+txbits = zeros(1,bits);  % transmit vector or freq bins  
+% make BPSK data vector 
+for k = 1:bits
+    if mbits(k) == '1'
+        txbits(k) = 1;  % bit one ,  zero phase:  exp(j * 0) = 1
+    else
+        txbits(k) = -1;  % bit zero,  180 phase:  exp(j*pi) = -1 
+    end
+end
+
+% make data vector bpsk,  1 = 1,  0 = -1
+xd = txbits;
+xd = [zeros(1,512)   xd    zeros(1,512) ];  %512 guard on each side
+
+
+
+
+
+% relate data vector to pilot vector
+xd = xp .* xd;  % e^a * e^b =  e^(a + b)
+
+
+
+
+
+% pilot time vector
+xp = ifftshift(xp);
+xp = ifft(xp);
+xp = xp / max(abs(xp));
+
+% data time vector
+xd = ifftshift(xd);
+xd = ifft(xd);
+xd = xd / max(abs(xd));
+
+% put the whole movie together
+xz = zeros(1,guardN);
+xt = [tdvec pream xz xp xp xd xd tdvec];
+
+% ensure 90 % bandwidth, reduce out of band signals
+hlpf = fir1(64,0.9);
+xt = filter(hlpf,1,xt);
+xt = xt / max(abs(xt));  % 1xN
+
+datafile = [ real(xt).'  imag(xt).' ];  %  Nx2
+
+
+% write iq file as wav file , use file in gnuradio flow blocks: 
+ audiowrite('c:\SDR_Work\ofdmtest300khzIQ1024Char.wav',datafile,fs,'BitsPerSample',16);
+
+
+% % make a sim iq rx file
+% 
+% xt = resample(xt,4,1);
+% N = length(xt);
+% nvec = randn(1,N) + 1i*randn(1,N);
+% 
+% xt = 10^(-10/20) * xt + 10^(-40/20) * nvec ; % create 20db SNR 
+% 
+% t = [1:N]/(4 * fs);
+% xt = xt .* exp(1i*2*pi*1235.45*t);  % apply freq offset  
+% fs = 4*fs;
+% datafile = [real(xt).' imag(xt).']; 
+% 
+% audiowrite('c:\SDR_Work\ofdmSimRX100khz256Char.wav',datafile,fs,'BitsPerSample',16);
+% 
+
+
+
+
+% sample rate 50nsec  of ARB .wv file
+clock = fs;
+rg = 1/clock;
+
+
+clocka = num2str(clock);
+clock = ['{CLOCK:' clocka '}'];
+clock2 = ['{CLOCK MARKER:' clocka '}'];
+
+
+
+
+
+data = xt;
+
+
+
+
+scale = 2^15 - 1;
+data = scale * data;
+data = round(data);
+data = int16(data);
+
+
+
+iqlength = length(data);
+
+ippvector = data;
+
+clear data
+
+
+
+
+% make file for R&S format  .wv  file  
+
+
+
+[filename, pathname] = uiputfile( '.wv', 'Save .wv  File  As:  ','D:\New folder\video_spectrum');
+
+x = [pathname filename];
+
+fid = fopen (x, 'w', 'l');
+
+fprintf(fid,'{TYPE: SMU-WV,0}');
+fprintf(fid,clock);
+fprintf(fid,clock2);
+fprintf(fid,'{LEVEL OFFS:0,0}');
+
+samples = ['{SAMPLES:' num2str(iqlength) '}'];
+% fprintf(fid,'{SAMPLES:600}');
+fprintf(fid,samples);
+
+
+
+
+
+% compute length for rs .wv file
+%  length  = number_of_IQpairs * 4  + 1  
+iqlength = iqlength*4 + 1;
+iqlength = num2str(iqlength);
+iqlength = ['{WAVEFORM-' iqlength ': #'];
+fprintf(fid,iqlength);
+
+
+    
+    
+   
+    ippvector = [ real(ippvector) ; imag(ippvector)];
+    ippvector = reshape(ippvector, 1 , []);
+    
+    
+    
+    
+    
+    
+    
+    
+    fwrite(fid, ippvector, 'int16' , 'l' );
+    
+
+
+% close file
+
+fprintf(fid,'}');
+fclose (fid);
+
+
+
+
+
+
+
+
+
+
+
+
